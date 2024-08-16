bfbf7c74ec05f85d0c9f28b9bf8fe93f04c2d453
Antonio Sebastiao Pedro <antoniosebastiaopedro19@gmail.com>
2024-08-16 15:11:41 +0100

    Add functions to swap a stack

---
+++ swap.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   swap.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ansebast <ansebast@student.42luanda.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/01 14:56:37 by ansebast          #+#    #+#             */
+/*   Updated: 2024/08/16 15:08:54 by ansebast         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <ft_printf/ft_printf.h>
+#include <libft/libft.h>
+
+void	swap(t_list **stack)
+{
+	t_list	*first;
+	t_list	*second;
+
+	if (*stack && (*stack)->next)
+	{
+		first = *stack;
+		second = (*stack)->next;
+		first->next = second->next;
+		second->next = first;
+		*stack = second;
+	}
+}
+
+void	sa(t_list **stack_a)
+{
+	swap(stack_a);
+	ft_printf("sa\n");
+}
+
+void	sb(t_list **stack_b)
+{
+	swap(stack_b);
+	ft_printf("sb\n");
+}
+
+void	ss(t_list **stack_a, t_list **stack_b)
+{
+	swap(stack_a);
+	swap(stack_b);
+	ft_printf("ss\n");
+}
