0728205ef06e7c9301c6cb8baccbfc733217c700
Antonio Sebastiao Pedro <antoniosebastiaopedro19@gmail.com>
2024-08-22 18:01:48 +0100

    Functions do rotate stacks

--- rotate.c
+++ rotate.c
@@ -6,42 +6,50 @@
 /*   By: ansebast <ansebast@student.42luanda.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/16 15:01:51 by ansebast          #+#    #+#             */
-/*   Updated: 2024/08/21 12:50:17 by ansebast         ###   ########.fr       */
+/*   Updated: 2024/08/22 18:00:41 by ansebast         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/libft.h"
 
-void	rotate(t_list **stack)
+void	ra(t_stack **a)
 {
-	t_list	*first;
-	t_list	*last;
+	t_stack	*first;
+	t_stack	*last;
 
-	if (*stack && (*stack)->next)
+	if (*a == NULL || (*a)->next == NULL)
 	{
-		first = *stack;
-		last = ft_lstlast(*stack);
-		*stack = first->next;
-		first->next = NULL;
-		last->next = first;
+		return ;
 	}
+	first = *a;
+	*a = first->next;
+	(*a)->previous = NULL;
+	last = ft_lstlast(a);
+	last->next = first;
+	first->previous = last;
+	first->next = NULL;
 }
 
-void	ra(t_list **stack_a)
+void	rb(t_stack **b)
 {
-	rotate(stack_a);
-	ft_putstr_fd("ra\n", 1);
+	t_stack	*first;
+	t_stack	*last;
+
+	if (*b == NULL || (*b)->next == NULL)
+	{
+		return ;
+	}
+	first = *b;
+	*b = first->next;
+	(*b)->previous = NULL;
+	last = ft_lstlast(b);
+	last->next = first;
+	first->previous = last;
+	first->next = NULL;
 }
 
-void	rb(t_list **stack_b)
+void	rr(t_stack **a, t_stack **b)
 {
-	rotate(stack_b);
-	ft_putstr_fd("rb\n", 1);
-}
-
-void	rr(t_list **stack_a, t_list **stack_b)
-{
-	rotate(stack_a);
-	rotate(stack_b);
-	ft_putstr_fd("rr\n", 1);
+	ra(a);
+	rb(b);
 }
