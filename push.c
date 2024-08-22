01e049607b369c951c23a06d4efd7c1adeb663be
Antonio Sebastiao Pedro <antoniosebastiaopedro19@gmail.com>
2024-08-22 18:02:02 +0100

    Functions do push stacks

--- push.c
+++ push.c
@@ -6,43 +6,55 @@
 /*   By: ansebast <ansebast@student.42luanda.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/01 14:59:10 by ansebast          #+#    #+#             */
-/*   Updated: 2024/08/21 12:50:52 by ansebast         ###   ########.fr       */
+/*   Updated: 2024/08/22 17:55:22 by ansebast         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/libft.h"
 
-void	push(t_list **src, t_list **dest)
+void	pa(t_stack **a, t_stack **b)
 {
-	t_list	*current;
+	t_stack	*top_b;
 
-	if (*src)
+	if (*b == NULL)
 	{
-		current = *src;
-		*src = (*src)->next;
-		current->next = *dest;
-		*dest = current;
+		return ;
 	}
+	top_b = *b;
+	*b = (*b)->next;
+	if (*b != NULL)
+	{
+		(*b)->previous = NULL;
+	}
+	top_b->next = *a;
+	if (*a != NULL)
+	{
+		(*a)->previous = top_b;
+	}
+	top_b->previous = NULL;
+	*a = top_b;
 }
 
-void	pa(t_list **stack_a, t_list **stack_b)
+void	pb(t_stack **a, t_stack **b)
 {
-	push(stack_b, stack_a);
-	ft_putstr_fd("pa\n", 1);
-}
+	if (*a == NULL)
+	{
+		return ;
+	}
 
-void	pb(t_list **stack_a, t_list **stack_b)
-{
-	push(stack_a, stack_b);
-	ft_putstr_fd("pb\n", 1);
-}
+	t_stack *top_a = *a;
+	*a = (*a)->next;
 
-// void push_operation(t_list **operations, char *operation)
-// {
-//     t_list *new_node = ft_lstnew(operation);
-//     if (new_node)
-//     {
-//         new_node->next = *operations;
-//         *operations = new_node;
-//     }
-// }
+	if (*a != NULL)
+	{
+		(*a)->previous = NULL;
+	}
+
+	top_a->next = *b;
+	if (*b != NULL)
+	{
+		(*b)->previous = top_a;
+	}
+	top_a->previous = NULL;
+	*b = top_a;
+}
